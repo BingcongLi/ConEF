f718889f7bac933599f0ad7917d7d9884fdeecb6
BingcongLi <42422410+BingcongLi@users.noreply.github.com>
2021-12-02 15:55:22 -0600

    Update get_comm_hooks.py

--- examples/ImageClassification/get_comm_hooks.py
+++ examples/ImageClassification/get_comm_hooks.py
@@ -1,9 +1,9 @@
 from torch.nn.parallel import DistributedDataParallel as ddp
-import torch.distributed.algorithms.ddp_comm_hooks as ddp_comm
 import communication.RandomBlock_hook as RandomBlock
 import communication.UnbiasedRandomBlock_hook as UnbiasedRandomBlock
 import communication.powerSGD_hook as powerSGD
 import communication.hPowerSGD_hook as hPowerSGD
+import communication.unbiasedMtrx_hook as UnbiasedMtrx
 
 
 def get_comm_hooks(args, net, gpu):
@@ -15,7 +15,6 @@ def get_comm_hooks(args, net, gpu):
     if EF or ConEF, return a the error vector or compressed error
     """
 
-    #support allreduce, ef_randomblock, conef_randomblock, unbiased_randomblock
 
     reducer_name = args.grad_reducer.lower()
     start_compression_iter = args.start_compression_iter
@@ -109,7 +108,20 @@ def get_comm_hooks(args, net, gpu):
         )
         hook = powerSGD.powerSGD_hook
         net.register_comm_hook(state, hook)
+    elif reducer_name == 'unbiased_mtrx':
+        matrix_approximation_rank = args.matrix_approximation_rank
+        start_compression_iter = args.start_compression_iter
+        min_compression_rate = args.min_compression_rate  # typically set to 2
+        state = UnbiasedMtrx.unbiasedMtrxState(
+                process_group=None,
+                matrix_approximation_rank=matrix_approximation_rank,
+                start_powerSGD_iter=start_compression_iter,
+                min_compression_rate=min_compression_rate,
+            )
+        hook = UnbiasedMtrx.UnbiasedMtrx_hook
+        net.register_comm_hook(state, hook)
     else:
         raise ValueError("Communication hook unsupported error.")
 
     return state
+
