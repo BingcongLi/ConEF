7547d9559f714de5881208975939284b2f89511f
BingcongLi <42422410+BingcongLi@users.noreply.github.com>
2021-12-02 12:27:12 -0600

    Add files via upload

---
+++ examples/ImageClassification/models/__init__.py
@@ -0,0 +1,9 @@
+from models.vgg import *
+from models.lenet import *
+from models.densenet import *
+from models.shufflenetv2 import *
+from models.resnet import *
+from models.resnext import *
+from models.mobilenetv2 import *
+from models.wideresnet import *
+
